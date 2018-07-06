c494127b5159afb7466898b8395f987f7d851604
Jeff Miscione <jeffmiscione@gmail.com>
2018-07-05 20:44:37 -0400

    Add files via upload

---
+++ compareTemplate.py
@@ -0,0 +1,7 @@
+import compareBoM
+
+print('hello')
+
+compareBoM.compare_bom_pdf('C:/Users/Jeff/Desktop/drawings/BoM.xlsx', 'C:/Users/Jeff/Desktop/drawings/pdfs')
+
+compareBoM.compare_bom_slddrw('C:/Users/Jeff/Desktop/drawings/BoM.xlsx', 'C:/PDM')
